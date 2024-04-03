cf170f350bffcc7aa03805c5250ac17a4d6aed7b
surya thangaraj <suryathangaraj95@gmail.com>
2024-04-03 18:07:27 +0530

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Feed from "@components/Feed";
 import PromptCard  from "@components/PromptCard"
+import {Suspense} from "react";
 
 const Home = () => (
   <section className='w-full flex-center flex-col'>
@@ -12,8 +13,9 @@ const Home = () => (
       Prompt.World is an open-source AI prompting tool for modern world to
       discover, create and share creative prompts
     </p>
-
+    <Suspense fallback={<>Loading...</>}>
     <Feed />
+    </Suspense>
    
   </section>
 );
